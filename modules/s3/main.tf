a9e4d8035ea4b5bff518f1ff87a50368a7895596
val-tina-r <valentina0519@gmail.com>
2026-04-28 20:26:16 -0500

    update

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -28,6 +28,8 @@ resource "aws_s3_bucket_public_access_block" "public_access_block" {
 resource "aws_s3_bucket_policy" "bucket_policy" {
     bucket = aws_s3_bucket.website.id
     
+    depends_on = [aws_s3_bucket_public_access_block.public_access_block]
+
     policy = jsonencode({
         Version = "2012-10-17"
         Statement = [
@@ -44,6 +46,7 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
 
 #subir el archivo index.html al bucket
 resource "aws_s3_object" "index_html" {
+
   bucket = aws_s3_bucket.website.id
   key    = "index.html"
   source = "index.html"
